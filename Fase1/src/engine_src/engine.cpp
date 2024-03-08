6609fa4a293f5a4ee89f16c81946db63e6469ee6
rui1479 <ruiribeiro2003.rr@gmail.com>
2024-03-08 18:25:33 +0000

    comentar cull_face

--- Fase1/src/engine_src/engine.cpp
+++ Fase1/src/engine_src/engine.cpp
@@ -318,7 +318,7 @@ int main(int argc, char** argv) {
     glutKeyboardFunc(keyboardFunc);
 
     glEnable(GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE);
+    //glEnable(GL_CULL_FACE);
 
     glutMainLoop();
 
